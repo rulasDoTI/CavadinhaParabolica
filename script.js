cc7b0add71b2ef1420193e521040151d3810bb7f
rulasDoTI <121449741+rulasDoTI@users.noreply.github.com>
2024-01-21 21:35:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function toggleMode() {
   // }
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/cavadinhaLogo.png")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/cavadinhaLogo.png")
   }
 }
